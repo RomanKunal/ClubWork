95d112c8d9b0c139de082405b682b8f2bf0d7e18
Kunal Singh <158199747+RomanKunal@users.noreply.github.com>
2025-10-03 13:31:07 +0530

    Change API URL to Render backend

--- clubconnect/src/api.js
+++ clubconnect/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://myapp-hifk.onrender.com/api';  // Render backend URL
 
 // Create axios instance
 const api = axios.create({
